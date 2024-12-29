c423fd2422fffc9ee64b9f0231c1e608c36e9831
Marcos Jianoto <marcos_jianotoo@hotmail.com>
2024-12-29 09:50:09 -0300

    commit impares1a1000

--- src/main/SomaImparesDe1a1000.java
+++ src/main/SomaImparesDe1a1000.java
@@ -4,29 +4,14 @@ public class SomaImparesDe1a1000 {
 
 	public static void main(String[] args) {
 
-		String palavraPalindromoSemFormatacao = "marcos";
-		String palavraPalindromo = palavraPalindromoSemFormatacao.toLowerCase();
-		String palavraInvertida = "";
+		Integer sumInteger = 0;
 
-		for (int i = palavraPalindromo.length() - 1; i >= 0; i--) {
-			palavraInvertida += palavraPalindromo.charAt(i);
-		}
-		int sum = 0;
-		for (int i = 0; i < palavraPalindromo.length(); i++) {
-			if (Character.toString(palavraPalindromo.charAt(i))
-					.equals(Character.toString(palavraInvertida.charAt(i)))) {
-				sum++;
+		for (int i = 0; i < 1000; i++) {
+			if (i % 2 != 0) {
+				sumInteger += i;
 			}
 		}
-
-
-		if (sum == palavraPalindromo.length()) {
-			System.out.println("palindromo");
-		} else {
-			System.out.println("não é um palindromo");
-		}
-		System.out.println(sum);
-		System.out.println(palavraPalindromo.length());
+		System.out.println(sumInteger);
 
 	}
 
